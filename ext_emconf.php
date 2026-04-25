e039a82be749400c8dc5ca19cb9372541884de78
Sven Wappler <wappler@wappler.systems>
2026-04-26 01:48:28 +0200

    Update TYPO3 dependency constraint to 14.0.0-14.3.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF['proxy'] = [
     'version' => '14.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.0.0-14.4.99',
+            'typo3' => '14.0.0-14.3.99',
         ],
         'conflicts' => [
         ],
