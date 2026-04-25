1d163140a492f6b055c30600e1c55f4aea269505
Sven Wappler <wappler@wappler.systems>
2026-04-26 01:43:04 +0200

    Refactor proxy plugin registration and update FlexForm configuration

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,14 +8,9 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 ExtensionUtility::registerPlugin(
     'proxy',
     'proxy',
-    'Proxy'
-);
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['proxy_proxy'] = 'pages,layout,select_key,recursive';
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['proxy_proxy'] = 'pi_flexform';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'proxy_proxy',
+    'Proxy',
+    null,
+    'plugins',
+    '',
     'FILE:EXT:proxy/Configuration/FlexForms/Settings.xml'
 );
